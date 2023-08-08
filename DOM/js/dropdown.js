cef6289e0e7f5fbb4a2e717b2c9dc9eec4c1fb54
ProfessionalProgrammer95 <rakshithav1358@gmail.com>
2023-08-08 15:49:57 +0530

    js

---
+++ DOM/js/dropdown.js
@@ -0,0 +1,27 @@
+let noSel = document.querySelector('#no');/* left side select option*/
+let yesSel = document.querySelector('#yes');/* right side select option*/
+
+let addBtn = document.querySelector('#add');
+let removeBtn = document.querySelector('#remove');
+
+
+//domTarget.addEventListener(event,callback)
+addBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    // alert('add btn clicked');
+    let selOpt = document.querySelectorAll("#no option:checked");
+        console.log('selected options =', selOpt);
+
+    let exstOpt = document.querySelectorAll('#yes option');
+        yesSel.replaceChildren(...selOpt,...exstOpt); /*Array -> parameters*/
+});
+removeBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    // alert('remove btn clicked');
+
+    let exstOpt = document.querySelectorAll("#yes option:checked");
+        // console.log('selected options =', exstOpt);
+
+    let selOpt = document.querySelectorAll('#no option');
+    noSel.replaceChildren(...exstOpt,...selOpt);
+});
